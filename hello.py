78c02ab5e72f6ab78057fae1ca9c93f1cdfd64a5
Malthe Jakobsen <malthe.jakobsen@sinful.com>
2025-08-21 15:19:35 +0200

    opsætning af filer

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello, GitHub!")
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loader variabler fra .env i projektroden
+
+print("Hej fra .venv!")
+print("API_KEY sat?" , bool(os.getenv("API_KEY")))
